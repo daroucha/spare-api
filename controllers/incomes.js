88784a074fbf04203c922710bc7ecf1460c9efa2
Daniel Aroucha <daniel.aroucha97@gmail.com>
2023-10-22 10:57:01 -0300

    Added simple CRUD to Incomes controller

--- controllers/incomes.js
+++ controllers/incomes.js
@@ -1,49 +1,121 @@
+const Income = require('../models/Income')
+
 // @desc    Get all incomes
 // @route   GET /api/v1/incomes
 // @access  Private
-exports.getIncomes = (req, res, next) => {
-  res.status(200).json({
-    success: true,
-    msg: `Show all incomes`,
-  })
+exports.getIncomes = async (req, res, next) => {
+  try {
+    const incomes = await Income.find()
+
+    res.status(200).json({
+      success: true,
+      count: incomes.length,
+      data: incomes,
+    })
+  } catch (err) {
+    res.status(400).json({
+      success: false,
+    })
+  }
 }
 
 // @desc    Get single income
 // @route   GET /api/v1/incomes/:id
 // @access  Private
-exports.getIncome = (req, res, next) => {
-  res.status(200).json({
-    success: true,
-    msg: `Show income ${req.params.id}`,
-  })
+exports.getIncome = async (req, res, next) => {
+  try {
+    const income = await Income.findById(
+      req.params.id
+    )
+
+    if (!income) {
+      return res.status(400).json({
+        success: false,
+      })
+    }
+
+    res.status(200).json({
+      success: true,
+      data: income,
+    })
+  } catch (err) {
+    res.status(400).json({
+      success: false,
+    })
+  }
 }
 
 // @desc    Create new income
 // @route   POST /api/v1/incomes
 // @access  Private
-exports.createIncome = (req, res, next) => {
-  res.status(200).json({
-    success: true,
-    msg: `Create a new income`,
-  })
+exports.createIncome = async (req, res, next) => {
+  try {
+    const income = await Income.create(req.body)
+
+    res.status(201).json({
+      success: true,
+      data: income,
+    })
+  } catch (err) {
+    res.status(400).json({
+      success: false,
+    })
+  }
 }
 
 // @desc    Update income
 // @route   PUT /api/v1/incomes/:id
 // @access  Private
-exports.updateIncome = (req, res, next) => {
-  res.status(200).json({
-    success: true,
-    msg: `Update income ${req.params.id}`,
-  })
+exports.updateIncome = async (req, res, next) => {
+  try {
+    const income = await Income.findByIdAndUpdate(
+      req.params.id,
+      req.body,
+      {
+        new: true,
+        runValidators: true,
+      }
+    )
+
+    if (!income) {
+      return res.status(400).json({
+        success: false,
+      })
+    }
+
+    res.status(200).json({
+      success: true,
+      data: income,
+    })
+  } catch (err) {
+    return res.status(400).json({
+      success: false,
+    })
+  }
 }
 
 // @desc    Delete income
 // @route   DELETE /api/v1/incomes/:id
 // @access  Private
-exports.deleteIncome = (req, res, next) => {
-  res.status(200).json({
-    success: true,
-    msg: `Delete income ${req.params.id}`,
-  })
+exports.deleteIncome = async (req, res, next) => {
+  try {
+    const income = await Income.findByIdAndDelete(
+      req.params.id
+    )
+
+    if (!income) {
+      return res.status(400).json({
+        success: false,
+      })
+    }
+
+    res.status(200).json({
+      success: true,
+      data: {},
+    })
+  } catch (err) {
+    return res.status(400).json({
+      success: false,
+    })
+  }
 }
--- server.js
+++ server.js
@@ -19,6 +19,9 @@ const incomes = require('./routes/incomes')
 // Initialize App
 const app = express()
 
+// Body req parser
+app.use(express.json())
+
 // Dev logging middleware
 if (process.env.NODE_ENV === 'development') {
   app.use(morgan('dev'))
