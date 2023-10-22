13c3a96e6dd0e05bc8a5c6f15a513f4cf2600cf6
Daniel Aroucha <daniel.aroucha97@gmail.com>
2023-10-22 10:14:55 -0300

    Added Income Model

---
+++ models/Income.js
@@ -0,0 +1,43 @@
+const mongoose = require('mongoose')
+
+const IncomeSchema = new mongoose.Schema({
+  name: {
+    type: String,
+    required: [true, 'Please add a name'],
+    trim: true,
+    maxlength: [
+      80,
+      'Name can not be more than 80 characters',
+    ],
+  },
+  slug: String,
+  amount: {
+    type: Number,
+    min: [1, 'Amount must be at least 1'],
+  },
+  recurrence: [
+    {
+      status: {
+        type: Boolean,
+        default: false,
+      },
+      date: {
+        type: Date,
+        default: Date.now,
+      },
+    },
+  ],
+  date: {
+    type: Date,
+    default: Date.now,
+  },
+  createdAt: {
+    type: Date,
+    default: Date.now,
+  },
+})
+
+module.exports = mongoose.model(
+  'Income',
+  IncomeSchema
+)
