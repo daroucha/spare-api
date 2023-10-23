09e6b0e187cf88d779c30a9e491f5e14de6bfd50
Daniel Aroucha <daniel.aroucha97@gmail.com>
2023-10-23 13:42:36 -0300

    Added config example

--- config/db.js
+++ config/db.js
@@ -1,9 +1,7 @@
 const mongoose = require('mongoose')
 
 const connectDB = async () => {
-  const conn = await mongoose.connect(
-    process.env.MONGO_URI
-  )
+  const conn = await mongoose.connect(process.env.MONGO_URI)
 
   console.log(
     `[SpareAPI] MongoDB Connected: ${conn.connection.host}`
