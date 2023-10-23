2be96ab305fd68d24a4a3468ffd1d2facba7ff39
Daniel Aroucha <daniel.aroucha97@gmail.com>
2023-10-23 14:42:23 -0300

    Added pagination

--- controllers/incomes.js
+++ controllers/incomes.js
@@ -13,7 +13,7 @@ exports.getIncomes = asyncHandler(
     const reqQuery = { ...req.query }
 
     // Fields to exclude for filtering
-    const removeFields = ['select', 'sort']
+    const removeFields = ['select', 'sort', 'page', 'limit']
 
     // Loop over remove fields and delete them from request query
     removeFields.forEach((param) => delete reqQuery[param])
@@ -44,12 +44,39 @@ exports.getIncomes = asyncHandler(
       query = query.sort('-createdAt')
     }
 
+    // Pagination
+    const page = parseInt(req.query.page, 10) || 1
+    const limit = parseInt(req.query.limit, 10) || 10
+    const startIndex = (page - 1) * limit
+    const endIndex = page * limit
+    const total = await Income.countDocuments()
+
+    query = query.skip(startIndex).limit(limit)
+
     // Executing query
     const incomes = await query
+
+    // Pagination result
+    const pagination = {}
+
+    if (endIndex < total) {
+      pagination.next = {
+        page: page + 1,
+        limit,
+      }
+    }
+
+    if (startIndex > 0) {
+      pagination.prev = {
+        page: page - 1,
+        limit,
+      }
+    }
 
     res.status(200).json({
       success: true,
       count: incomes.length,
+      pagination,
       data: incomes,
     })
   }
