d160b17a1e6d2a317e2cc8b73fffc5dd108840df
Daniel Aroucha <daniel.aroucha97@gmail.com>
2023-10-24 11:25:59 -0300

    Added password reset functionality

--- routes/auth.js
+++ routes/auth.js
@@ -3,6 +3,8 @@ const {
   register,
   login,
   getMe,
+  forgotPassword,
+  resetPassword,
 } = require('../controllers/auth')
 
 const router = express.Router()
@@ -12,5 +14,7 @@ const { protect } = require('../middleware/auth')
 router.post('/register', register)
 router.post('/login', login)
 router.get('/me', protect, getMe)
+router.post('/forgotpassword', forgotPassword)
+router.put('/resetpassword/:resettoken', resetPassword)
 
 module.exports = router
