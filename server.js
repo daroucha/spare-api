98177e8dfc3d9f02a2849993a3bea266836b7bb4
Daniel Aroucha <daniel.aroucha97@gmail.com>
2023-10-25 17:51:35 -0300

    Added rate limit

--- server.js
+++ server.js
@@ -6,6 +6,8 @@ const cookieParser = require('cookie-parser')
 const mongoSanitize = require('express-mongo-sanitize')
 const helmet = require('helmet')
 const xss = require('xss-clean')
+const rateLimit = require('express-rate-limit')
+const hpp = require('hpp')
 
 const errorHandler = require('./middleware/error')
 const connectDB = require('./config/db')
@@ -44,6 +46,16 @@ app.use(helmet())
 
 // Prevent XSS attacks
 app.use(xss())
+
+// Rate limiting
+const limiter = rateLimit({
+  windowMs: 10 * 60 * 1000, // 10 minutes
+  max: 100,
+})
+app.use(limiter)
+
+// Prevent http param pollution
+app.use(hpp())
 
 // Mount routers
 app.use('/api/v1/incomes', incomes)
