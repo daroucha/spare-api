970d7081be6f48e42d6bc5d54ed38f9784166dfa
Daniel Aroucha <daniel.aroucha97@gmail.com>
2023-11-08 18:15:23 -0300

    Added Debt Model

---
+++ controllers/debts.js
@@ -0,0 +1,31 @@
+const ErrorResponse = require('../utils/errorResponse')
+const asyncHandler = require('../middleware/async')
+const Debt = require('../models/Debt')
+
+// @desc    Get all debts
+// @route   GET /api/v1/debts
+// @access  Private
+exports.getDebts = asyncHandler(async (req, res, next) => {
+  res.status(200).json(res.advancedResults)
+})
+
+// @desc    Get single debt
+// @route   GET /api/v1/debts/:id
+// @access  Private
+exports.getDebt = asyncHandler(async (req, res, next) => {
+  const debt = await Debt.findById(req.params.id)
+
+  if (!debt) {
+    return next(
+      new ErrorResponse(
+        `Debt not found with id of ${req.params.id}`,
+        404
+      )
+    )
+  }
+
+  res.status(200).json({
+    success: true,
+    data: debt,
+  })
+})
---
+++ models/Debt.js
@@ -0,0 +1,56 @@
+const mongoose = require('mongoose')
+
+const DebtSchema = new mongoose.Schema(
+  {
+    name: {
+      type: String,
+      required: [true, 'Please add a name'],
+      trim: true,
+      maxlength: [
+        80,
+        'Name can not be more than 80 characters',
+      ],
+    },
+    amount: {
+      type: Number,
+      required: [true, 'Please add a amount'],
+      min: [1, 'Amount must be at least 1'],
+    },
+    status: {
+      type: Boolean,
+      default: false,
+    },
+    debtDate: {
+      type: Date,
+      default: Date.now,
+    },
+    fees: {
+      status: Boolean,
+      amount: Number,
+      type: {
+        type: String,
+        enum: ['compound', 'simple'],
+      },
+    },
+    createdAt: {
+      type: Date,
+      default: Date.now,
+    },
+    user: {
+      type: mongoose.Schema.ObjectId,
+      ref: 'User',
+      required: true,
+    },
+  },
+  {
+    toJSON: { virtuals: true },
+    toObject: { virtuals: true },
+  }
+)
+
+// Calculate fees
+DebtSchema.virtual('finalAmount').get(function () {
+  return this.amount
+})
+
+module.exports = mongoose.model('Debt', DebtSchema)
