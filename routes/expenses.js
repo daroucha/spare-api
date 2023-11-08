8c25557ba4bf97b59727c5cd57dd4a05a76f2a74
Daniel Aroucha <daniel.aroucha97@gmail.com>
2023-11-08 14:30:04 -0300

    Added expenses route

---
+++ routes/expenses.js
@@ -0,0 +1,33 @@
+const express = require('express')
+const {
+  getExpenses,
+  getExpense,
+  createExpense,
+  updateExpenses,
+  deleteExpense,
+} = require('../controllers/expenses')
+
+const Expense = require('../models/Expense')
+const advancedResults = require('../middleware/advancedResults')
+
+const router = express.Router()
+
+const { protect, authorize } = require('../middleware/auth')
+
+router
+  .route('/')
+  .get(
+    protect,
+    authorize('user', 'watcher'),
+    advancedResults(Expense),
+    getExpenses
+  )
+  .post(protect, authorize('user'), createExpense)
+
+router
+  .route('/:id')
+  .get(protect, authorize('user', 'watcher'), getExpense)
+  .put(protect, authorize('user'), updateExpenses)
+  .delete(protect, authorize('user'), deleteExpense)
+
+module.exports = router
